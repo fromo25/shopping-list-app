233b2f179d2b3a320a5b8527275d4e98c63e97f3
fromo25 <romo3d@gmail.com>
2016-05-14 15:48:05 -0500

    Add finished functionality

--- js/app.js
+++ js/app.js
@@ -1,13 +1,33 @@
 $(document).ready(function() {
-	$('.my-form').on('submit',function(e) {
-    e.preventDefault();
-    var userData = $('.user-data').val();
-  
-  	$('ul').append('<li class="current-item"><input type=checkbox class="check-box">' + ' ' + userData + '<img class="x-mark" src="images/back-x.svg"></li>');
-});
 
-$('ul').on('click', '.x-mark', function(event){
-		$(this).closest('li').remove();
-	});
+    var counter = 2;
+
+
+    $('.my-form').on('submit', function(e) {
+        e.preventDefault();
+        var userData = $('.user-data').val();
+
+        if (userData === "") {
+            alert('Please enter an item!')
+        } else {
+        $('ul').append('<li class="current-item"><input type=checkbox class="check-box">' + ' ' + userData + '<img class="x-mark" src="images/back-x.svg"></li>');
+        $('.user-data').val('');
+        }
+
+        counter++;
+        console.log(counter);
+
+        /*	if (counter >= 8) {
+            $('ul').css("overflow-y", "scroll");
+        }	*/
+
+    });
+
+    $('ul').on('click', '.x-mark', function(e) {
+        $(this).closest('li').remove();
+        /*add opposite direction here*/
+        counter--;
+        console.log(counter);
+    });
 
 });
